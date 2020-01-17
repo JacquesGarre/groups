a4c5f54651630d5a16570888dfc1be9a51794e05
Jaks <jcqs.gr@gmail.com>
2020-01-17 12:40:07 +0100

    Refactor

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ class Classe(object):
 
     def __init__(self, csv, max_persons_by_group):
         self.liste_apprenants = self.parseCsv(csv)
-        self.max_persons_by_group = max_persons_by_group
-        self.groups = self.createGroups()
+        self.__max_persons_by_group = max_persons_by_group
+        self.groups = self.__createGroups()
 
-    def createGroups(self):
-        groups_count = len(self.liste_apprenants) // self.max_persons_by_group + 1
+    def __createGroups(self):
+        groups_count = len(self.liste_apprenants) // self.__max_persons_by_group + 1
         random.shuffle(self.liste_apprenants)
         groups = np.array_split(self.liste_apprenants, groups_count)
         return groups
@@ -22,17 +22,16 @@ class Classe(object):
         file_path = os.path.join(os.path.dirname(__file__), csv_filename)
         with open(file_path, encoding="utf8") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in enumerate(csv_reader):
-                liste_apprenants.append(row[1][0] + ' ' + row[1][1])
+            for row in csv_reader:
+                liste_apprenants.append(row[0] + ' ' + row[1])
         return liste_apprenants
 
     def printTables(self):
         date = datetime.datetime.now().strftime('%d/%m/%Y')
         print("\nAujourd'hui, {} :\n".format(date))
         for i, group in enumerate(self.groups):
-            print(
-                "La table {} sera composée de : {}".format(i, ", ".join(map(str, group)))
-            )
+            persons = ", ".join(map(str, group[:-1])) + " et " + str(group[-1])
+            print("La table {} sera composée de : {}".format(i, persons))
         print("\n")
 
                 
